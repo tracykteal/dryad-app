de13adbabbbd8efe2746ad6498e25bf78762a45b
Ryan Scherle <ryan@scherle.org>
2020-03-31 04:08:04 +0000

    log

--- stash/stash_api/app/controllers/stash_api/datasets_controller.rb
+++ stash/stash_api/app/controllers/stash_api/datasets_controller.rb
@@ -111,7 +111,7 @@ module StashApi
     # get /datasets/<id>/download
     def download
       res = @stash_identifier.latest_viewable_resource(user: @user)
-
+      Rails.logger("res #{res.id}, current_user #{current_user.id}")
       if res.may_download?(ui_user: current_user)
         @version_streamer.download(resource: @res) do
           redirect_to landing_show_path(id: @res.identifier_str, big: 'showme') # if it's an async
